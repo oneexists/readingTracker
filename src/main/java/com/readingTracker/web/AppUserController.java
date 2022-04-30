237929c6401791b3c5e4b30411a284dfb15a3e73
Skylar Lynner <skylar.lynner@gmail.com>
2022-04-29 21:55:58 -0500

    add model assembler

--- src/main/java/com/readingTracker/web/AppUserController.java
+++ src/main/java/com/readingTracker/web/AppUserController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,39 +19,49 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 
-import com.readingTracker.data.repository.AppUserRepository;
+import com.readingTracker.service.AppUserService;
 import com.readingTracker.web.domain.AppUserModelAssembler;
 import com.readingTracker.data.entity.AppUser;
 
 @RestController @RequestMapping("/users")
 public class AppUserController {
-	private final AppUserRepository repository;
+	private final AppUserService service;
 	private final AppUserModelAssembler assembler;
 	
 	@Autowired
-	public AppUserController(AppUserRepository appUserRepository, AppUserModelAssembler appUserModelAssembler) {
-		this.repository = appUserRepository;
+	public AppUserController(AppUserService service, AppUserModelAssembler appUserModelAssembler) {
+		this.service = service;
 		this.assembler = appUserModelAssembler;
 	}
 	
+	@GetMapping("/all")
+	public CollectionModel<EntityModel<AppUser>> all() {
+		List<EntityModel<AppUser>> users = service.getAllUsers().stream()
+				.map(assembler::toModel)
+				.collect(Collectors.toList());
+		return CollectionModel.of(users, linkTo(methodOn(AppUserController.class).all()).withSelfRel());
+	}
+	
 	@PostMapping("/save")
-	public ResponseEntity<?> save(@RequestBody AppUser newUser) {
-		EntityModel<AppUser> entityModel = assembler.toModel(repository.save(newUser));
+	public ResponseEntity<EntityModel<AppUser>> save(@RequestBody AppUser newUser) {
+		EntityModel<AppUser> entityModel = assembler.toModel(service.saveUser(newUser));
 		return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
 	}
 	
 	@GetMapping("{id}")
 	public EntityModel<AppUser> get(@PathVariable Long id) {
-		AppUser user = repository.getById(id);
+		AppUser user = service.getUser(id);
 		return assembler.toModel(user);
 	}
-	
-	@GetMapping("/all")
-	public CollectionModel<EntityModel<AppUser>> all() {
-		List<EntityModel<AppUser>> users = repository.findAll().stream()
-				.map(assembler::toModel)
-				.collect(Collectors.toList());
-		return CollectionModel.of(users, linkTo(methodOn(AppUserController.class).all()).withSelfRel());
-	}
 
+	@PostMapping("/update")
+	public ResponseEntity<EntityModel<AppUser>> update(@RequestBody AppUser appUser) {
+		EntityModel<AppUser> entityModel = assembler.toModel(service.updateUser(appUser));
+		return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
+	}
+	
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
 }
