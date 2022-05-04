2da72223b452ab9dde10e54626c3da02457bbc14
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-04 11:06:44 -0500

    add reading status state

--- src/main/java/com/readingTracker/web/domain/LogModelAssembler.java
+++ src/main/java/com/readingTracker/web/domain/LogModelAssembler.java
@@ -10,13 +10,22 @@ import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 import com.readingTracker.data.entity.Log;
+import com.readingTracker.data.entity.ReadingStatus;
 import com.readingTracker.web.LogController;
 
 @Component
 public class LogModelAssembler implements RepresentationModelAssembler<Log, EntityModel<Log>> {
 	@Override
 	public EntityModel<Log> toModel(Log log) {
-		return EntityModel.of(log, linkTo(methodOn(LogController.class).get(log.getId())).withSelfRel(),
-				linkTo(methodOn(LogController.class).all()).withRel("books"));
+		EntityModel<Log> logModel = EntityModel.of(log, linkTo(methodOn(LogController.class).get(log.getId())).withSelfRel(),
+				linkTo(methodOn(LogController.class).all()).withRel("logs"));
+		if (log.getStatus() == ReadingStatus.IN_PROGRESS) {
+			logModel.add(linkTo(methodOn(LogController.class).deleteLog(log.getId())).withRel("delete"));
+			logModel.add(linkTo(methodOn(LogController.class).updateLog(log, log.getId())).withRel("update"));
+		}
+		if (log.getStatus() == ReadingStatus.FINISHED) {
+			logModel.add(linkTo(methodOn(LogController.class).deleteLog(log.getId())).withRel("delete"));
+		}
+		return logModel;
 	}
 }
