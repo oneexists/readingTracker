cac2b40b43200535772de3e89c34411dd15b8f7c
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-06 13:51:20 -0500

    add book service and populate model with all books for index page mapping

--- src/main/java/com/readingTracker/web/PageController.java
+++ src/main/java/com/readingTracker/web/PageController.java
@@ -1,13 +1,22 @@
 package com.readingTracker.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.readingTracker.service.BookService;
 import com.readingTracker.web.dto.AppUserRegistrationDto;
 
 @Controller
 public class PageController {
+	private final BookService bookService;
+
+	@Autowired
+	public PageController(BookService bookService) {
+		this.bookService = bookService;
+	}
+
 	@GetMapping("/user/login")
 	public String login(Model model) {
 		model.addAttribute(new AppUserRegistrationDto());
@@ -15,7 +24,8 @@ public class PageController {
 	}
 
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("books", bookService.getAllBooks());
 		return "index";
 	}
 
