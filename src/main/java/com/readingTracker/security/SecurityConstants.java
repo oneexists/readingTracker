509cd487f20470cdc11cabe5b37da20e2788491e
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-06 04:13:28 -0500

    update public and user urls

--- src/main/java/com/readingTracker/security/SecurityConstants.java
+++ src/main/java/com/readingTracker/security/SecurityConstants.java
@@ -1,17 +1,17 @@
 package com.readingTracker.security;
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 432_000_000; // 5 days expressed in milliseconds
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
-    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
-    public static final String TRACKERS_APPLICATION = "Trackers Application";
-    public static final String TRACKERS_ADMINISTRATION = "User Management Portal";
-    public static final String AUTHORITIES = "authorities";
-    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
-    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
-    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
-    public static final String[] PUBLIC_URLS = { "/login", "/" };
-    public static final String[] USER_URLS = { "/", "/books/**" };
-    public static final String[] MANAGER_URLS = { "/users/**" };
+	public static final long EXPIRATION_TIME = 432_000_000; // 5 days expressed in milliseconds
+	public static final String TOKEN_PREFIX = "Bearer ";
+	public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+	public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+	public static final String TRACKERS_APPLICATION = "Trackers Application";
+	public static final String TRACKERS_ADMINISTRATION = "User Management Portal";
+	public static final String AUTHORITIES = "authorities";
+	public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+	public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+	public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+	public static final String[] PUBLIC_URLS = { "/registration**", "/login", "/user/login**" };
+	public static final String[] USER_URLS = { "/", "/api/**" };
+	public static final String[] MANAGER_URLS = { "/users/**" };
 }
