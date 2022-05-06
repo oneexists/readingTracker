2ced6766fc3d08e4a06880b2c570c43682c5e53f
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-06 04:19:17 -0500

    support form login and registration

--- src/main/java/com/readingTracker/service/AppUserService.java
+++ src/main/java/com/readingTracker/service/AppUserService.java
@@ -1,18 +1,29 @@
 package com.readingTracker.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.readingTracker.data.entity.AppUser;
+import com.readingTracker.web.dto.AppUserRegistrationDto;
 
 public interface AppUserService extends UserDetailsService {
 	AppUser saveUser(AppUser user);
+
 	UserDetails loadUserByUsername(String username);
+
 	AppUser getUser(Long id);
+
 	List<AppUser> getAllUsers();
+
 	AppUser updateUser(AppUser appUser);
+
 	void delete(Long id);
+
+	AppUser registerUser(AppUserRegistrationDto appUserDto);
+
+	Optional<AppUser> findByUsername(String username);
 
 }
--- src/main/java/com/readingTracker/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/readingTracker/service/impl/AppUserServiceImpl.java
@@ -1,16 +1,17 @@
 package com.readingTracker.service.impl;
 
+import static java.util.Arrays.stream;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
-import static java.util.Arrays.stream;
-
-import java.util.List;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,38 +19,45 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.readingTracker.data.entity.AppUser;
+import com.readingTracker.data.entity.UserRole;
 import com.readingTracker.data.repository.AppUserRepository;
 import com.readingTracker.service.AppUserService;
 import com.readingTracker.service.exceptions.AppUserNotFoundException;
+import com.readingTracker.web.dto.AppUserRegistrationDto;
 
-@Service @Transactional
+@Service
+@Transactional
 public class AppUserServiceImpl implements AppUserService {
 	private final AppUserRepository appUserRepository;
 	private final PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	public AppUserServiceImpl(AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
 		this.appUserRepository = appUserRepository;
 		this.passwordEncoder = passwordEncoder;
 	}
-	
+
 	@Override
 	public AppUser saveUser(AppUser user) {
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		return appUserRepository.save(user);			
+		return appUserRepository.save(user);
 	}
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		AppUser appUser = appUserRepository.findByUsername(username);
-		if (appUser == null) {
-			throw new UsernameNotFoundException("User not found in database.");
-		}
+		AppUser appUser = appUserRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("Invalid username or password."));
 		String[] claims = appUser.getUserRole().getAuthorities();
-		Set<SimpleGrantedAuthority> authorities = stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
+		Set<SimpleGrantedAuthority> authorities = stream(claims).map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toSet());
 		return new User(appUser.getUsername(), appUser.getPassword(), authorities);
 	}
-	
+
+	@Override
+	public Optional<AppUser> findByUsername(String username) {
+		return appUserRepository.findByUsername(username);
+	}
+
 	@Override
 	public AppUser getUser(Long id) {
 		return appUserRepository.findById(id).orElseThrow(() -> new AppUserNotFoundException(id));
@@ -62,22 +70,28 @@ public class AppUserServiceImpl implements AppUserService {
 
 	@Override
 	public AppUser updateUser(AppUser appUser) {
-		return appUserRepository.findById(appUser.getUserId())
-				.map(user -> {
-					user.setName(appUser.getName());
-					user.setUsername(appUser.getUsername());
-					user.setPassword(appUser.getPassword());
-					user.setDateOfBirth(appUser.getDateOfBirth());
-					return appUserRepository.saveAndFlush(user);
-				})
-				.orElseGet(() -> {
-					appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
-					return appUserRepository.save(appUser);
-				});
+		return appUserRepository.findById(appUser.getUserId()).map(user -> {
+			user.setName(appUser.getName());
+			user.setUsername(appUser.getUsername());
+			user.setPassword(appUser.getPassword());
+			user.setDateOfBirth(appUser.getDateOfBirth());
+			return appUserRepository.saveAndFlush(user);
+		}).orElseGet(() -> {
+			appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
+			return appUserRepository.save(appUser);
+		});
 	}
 
 	@Override
 	public void delete(Long id) {
 		appUserRepository.deleteById(id);
+	}
+
+	@Override
+	public AppUser registerUser(AppUserRegistrationDto appUserDto) {
+		AppUser newUser = new AppUser(appUserDto.getName(), appUserDto.getUsername(),
+				passwordEncoder.encode(appUserDto.getPassword()), LocalDate.parse(appUserDto.getDateOfBirth()),
+				UserRole.ROLE_USER);
+		return appUserRepository.save(newUser);
 	}
 }
