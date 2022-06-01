86d8c8524a1e8bc6716d74652380006fd5a74710
Skylar Lynner <skylar.lynner@gmail.com>
2022-05-31 23:19:27 -0500

    remove command line runner and add application builder

--- src/main/java/com/readingTracker/ReadingTrackerApplication.java
+++ src/main/java/com/readingTracker/ReadingTrackerApplication.java
@@ -1,29 +1,22 @@
 package com.readingTracker;
 
-import java.time.LocalDate;
-
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.readingTracker.data.entity.AppUser;
-import com.readingTracker.data.entity.Author;
-import com.readingTracker.data.entity.Book;
-import com.readingTracker.data.entity.UserRole;
-import com.readingTracker.service.AppUserService;
-import com.readingTracker.service.AuthorService;
-import com.readingTracker.service.BookService;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableEncryptableProperties
-public class ReadingTrackerApplication {
+public class ReadingTrackerApplication extends SpringBootServletInitializer {
+	private static Class<ReadingTrackerApplication> applicationClass = ReadingTrackerApplication.class;
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
@@ -46,15 +39,9 @@ public class ReadingTrackerApplication {
 		return encryptor;
 	}
 
-	@Bean
-	CommandLineRunner run(AppUserService appUserService, AuthorService authorService, BookService bookService) {
-		return args -> {
-			appUserService
-					.saveUser(new AppUser("Jesse", "user", "pass", LocalDate.now().minusYears(27), UserRole.ROLE_USER));
-			authorService.saveAuthor(new Author("Oscar Wilde"));
-			bookService.saveBook(new Book("The Picture of Dorian Gray", authorService.getAllAuthors().get(0), "EN", 254,
-					appUserService.findByUsername("user").get()));
-		};
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
 	}
 
 	public static void main(String[] args) {
