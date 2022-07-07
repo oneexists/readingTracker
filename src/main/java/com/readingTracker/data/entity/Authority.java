174ee9518ed82f909874a615ac8d7996a215b7f9
Skylar Lynner <skylar.lynner@gmail.com>
2022-07-07 14:12:42 -0500

    removeo unused roles and authorities

--- src/main/java/com/readingTracker/data/entity/Authority.java
+++ src/main/java/com/readingTracker/data/entity/Authority.java
@@ -2,9 +2,7 @@ package com.readingTracker.data.entity;
 
 public class Authority {
 	public static final String[] USER_AUTHORITIES = { "user:read" };
-	public static final String[] HR_AUTHORITIES = { "user:read", "user:update" };
 	public static final String[] MANAGER_AUTHORITIES = { "user:read", "user:update" };
-	public static final String[] ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update" };
-	public static final String[] SUPER_ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update", "user:delete" };
+	public static final String[] ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update", "user:delete" };
 
 }
--- src/main/java/com/readingTracker/data/entity/Role.java
+++ src/main/java/com/readingTracker/data/entity/Role.java
@@ -1,10 +1,11 @@
 package com.readingTracker.data.entity;
 
-import static com.readingTracker.data.entity.Authority.*;
+import static com.readingTracker.data.entity.Authority.ADMIN_AUTHORITIES;
+import static com.readingTracker.data.entity.Authority.MANAGER_AUTHORITIES;
+import static com.readingTracker.data.entity.Authority.USER_AUTHORITIES;
 
 public enum Role {
-	ROLE_USER(USER_AUTHORITIES), ROLE_HR(HR_AUTHORITIES), ROLE_MANAGER(MANAGER_AUTHORITIES),
-	ROLE_ADMIN(ADMIN_AUTHORITIES), ROLE_SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES);
+	ROLE_USER(USER_AUTHORITIES), ROLE_MANAGER(MANAGER_AUTHORITIES), ROLE_ADMIN(ADMIN_AUTHORITIES);
 
 	private final String[] authorities;
 
