47bf50ed3231ed686c9d18598bd847b2e635acf5
Skylar Lynner <skylar.lynner@gmail.com>
2022-07-07 14:13:14 -0500

    specify cross origin source for react front end

--- src/main/java/com/readingTracker/web/AuthenticationController.java
+++ src/main/java/com/readingTracker/web/AuthenticationController.java
@@ -31,7 +31,7 @@ import com.readingTracker.web.dto.AppUserRegistrationDto;
 import com.readingTracker.web.dto.JwtResponse;
 import com.readingTracker.web.dto.MessageResponse;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
